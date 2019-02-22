8120fa36bc6c3fa2ce0fb552cec5de39df1188c2
eddy <eddyyonnie@gmail.com>
2019-02-22 17:58:28 +0300

    added content to html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).read(function(){
+ jQuery('h1').click(function(){
+    alert("This is a heading tag")
+  });
+
+  jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
+});
